ce06aca9babeaa05162efe33dd11cdafc757f63a
Nob Yuasys <yuasys@gmail.com>
2024-12-18 01:41:55 +0900

    Build My first Astro Island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://yuasys-astro-tutorial-01.netlify.app/"
+  site: "https://yuasys-astro-tutorial-01.netlify.app/",
+  integrations: [preact()]
 });
